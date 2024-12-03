2dbaae036e07624b81be695f3d746b548b0dbfde
mosheadd <babichroisy22@gmail.com>
2024-12-03 11:21:37 +0300

    add bisection alg realization

--- MKP/MKP.cpp
+++ MKP/MKP.cpp
@@ -20,12 +20,74 @@ namespace mkp
 
 	}
 
-
 	float SpaceObject::get_e() { return e; }
 	float SpaceObject::get_a() { return a; }
 	float SpaceObject::get_n() { return n; }
 
 	int SpaceObject::get_T() { return T; }
+
+
+	float KeplerEquation(float M, float e, float E) { return E - e * sin(E) - M; }
+
+
+	float bisection(float M, float e, float epsilon, int max_it)
+	{
+
+		struct abPares
+		{
+
+			float a;
+			float b;
+
+			abPares(float a, float b) : a(a), b(b) {}
+
+		};
+
+
+		float a = 0;
+		float b = 2 * PI;
+
+		std::vector<abPares> seqOfSegments_ab;
+
+
+		for (int i = 0; i < max_it; i++)
+		{
+
+			float E = (a + b) / 2;
+
+			float f = KeplerEquation(M, e, E);
+
+			if (f == 0)
+				return E;
+
+			float fa = KeplerEquation(M, e, a);
+			float fb = KeplerEquation(M, e, b);
+
+			if (fa * fb < 0)
+				seqOfSegments_ab.push_back(abPares(a, b));
+
+
+			if (fa * f < 0)
+				b = E;
+			else
+				a = E;
+
+
+			if (abs(b - a) < 2 * epsilon)
+			{
+
+				std::cout << "Ќе достигнуто нужного количества итераций.\n";
+				break;
+
+			}
+
+		}
+
+		abPares retValue = seqOfSegments_ab[seqOfSegments_ab.size() - 1];
+
+		return (retValue.a + retValue.b) / 2;
+
+	}
 
 
 }
