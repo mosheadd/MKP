05a141ec4f606ed427d923eed7d9739ce254a48b
mosheadd <babichroisy22@gmail.com>
2024-12-03 10:52:41 +0300

    inittial code

---
+++ MKP/MKP.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "cmath"
+
+
+namespace mkp
+{
+
+
+	const float PI = 3.1459;
+	const float kappa = 1.618;
+	const float G = 6.67e-11;
+
+
+	class SpaceObject
+	{
+
+		float e;
+		float a;
+
+		int mass;
+
+	};
+
+}
